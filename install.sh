930f50418618a12c0da9c08f1ea08376a5e6e953
mojtabasadegh10 <120686154+mojtabasadegh10@users.noreply.github.com>
2024-06-04 16:17:00 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ rm -rf 3xui-multi-protocol
 
 if [[ "${release}" == "debian" ]]; then
 apt install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
+git clone https://github.com/mojtabasadegh10/3xui-multi-protocol.git
   cd 3xui-multi-protocol/3xui-multi-protocol
   wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb 
   dpkg -i packages-microsoft-prod.deb 
@@ -25,7 +25,7 @@ git clone https://github.com/M4mmad/3xui-multi-protocol.git
 elif [[ "${release}" == "centos" ]]; then 
 dnf install wget
 dnf install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
+git clone https://github.com/mojtabasadegh10/3xui-multi-protocol.git
  cd 3xui-multi-protocol/3xui-multi-protocol
   sudo dnf install dotnet-sdk-7.0
   sudo dnf install aspnetcore-runtime-7.0
@@ -43,7 +43,7 @@ git clone https://github.com/M4mmad/3xui-multi-protocol.git
 elif [[ "${release}" == "ubuntu" ]]; then
 declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 apt install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
+git clone https://github.com/mojtabasadegh10/3xui-multi-protocol.git
  cd 3xui-multi-protocol/3xui-multi-protocol
  wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
  dpkg -i packages-microsoft-prod.deb
@@ -64,7 +64,7 @@ fi
 
  else
  apt install git
-git clone https://github.com/M4mmad/3xui-multi-protocol.git
+git clone https://github.com/mojtabasadegh10/3xui-multi-protocol.git
   wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
   chmod +x ./dotnet-install.sh
   ./dotnet-install.sh --channel 7.0
